5eae77ce65388dbe57f923ec54037b25edd4f502
louzenghui <1061677442@qq.com>
2019-08-20 17:18:55 +0800

    update-8-20

---
+++ build/precache-manifest.6833326ea371c0595f4a82972680bd6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641b42c56de5928af8d098098fb5a471",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8822095ebb9fe12da4b8",
+    "url": "./static/css/main.c45b264e.chunk.css"
+  },
+  {
+    "revision": "2f5d026bfaf8c07e4ba4",
+    "url": "./static/js/2.456ecfd1.chunk.js"
+  },
+  {
+    "revision": "8822095ebb9fe12da4b8",
+    "url": "./static/js/main.e1e2edca.chunk.js"
+  },
+  {
+    "revision": "f97b4c0f0239375c2b8b",
+    "url": "./static/js/runtime~main.c380e675.js"
+  }
+]);
