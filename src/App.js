463c311ea3669401f53f2cca4a7ea9ae68670c75
louzenghui <1061677442@qq.com>
2019-08-20 16:58:56 +0800

    add-8-20

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import PictureSelect from './PictureSelect';
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.updateState= this.updateState.bind(this);
+    this.state={
+      values:[]
+    }
+    this.pictures=[{
+      id: '1',
+      name: 'foo',
+      url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+    },
+    {
+      id: '2',
+      name: 'foo',
+      url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+    },
+    {
+      id: '3',
+      name: 'foo',
+      url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+    },]
+  }
+  updateState = (endValue) => {
+    this.setState({
+      values: endValue
+    })
+  }
+  render(){
+    const {values} = this.state;
+    return(
+      <div className="App">
+        <PictureSelect pictures={this.pictures} values={values} onChange={(endValue)=>this.updateState(endValue)}/>
+    </div>
+    )
+  }
+}
+
---
+++ src/PictureSelect.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+
+
+export default class PictureSelect extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            arr : this.props.values
+        }
+    }
+    hadleAll = () =>{
+        const {arr} = this.state;
+        let checkbox = document.getElementById('checkInput');
+        let arrCheck = [];
+        if(!checkbox.checked){
+            this.setState({
+                arr:[]
+            })
+        }
+        else{
+            this.props.pictures.map(item=>{
+                arrCheck.push(item.id)
+            })
+            this.setState({
+                arr:arrCheck
+            })
+        }
+        this.props.onChange(arr);
+    }
+    hadleCheck = (e,value) =>{
+        const { arr } = this.state;
+        if(arr.indexOf(value)!=-1){
+            arr.splice(arr.indexOf(arr.indexOf(value)), 1)
+        }
+        else{
+            arr.push(value);
+        }
+        this.setState({
+            arr
+        })
+        this.props.onChange(arr);
+    }
+    render() {
+        const {pictures} = this.props;
+        const {arr} = this.state;
+        return (
+             <div className = "App">
+                <div className="checkAll">
+                    <input id="checkInput" checked={arr.indexOf(pictures[0].id)!==-1&&arr.indexOf(pictures[1].id)!==-1&&arr.indexOf(pictures[2].id)!==-1?"checked":""} onChange={this.hadleAll} type="checkbox" value="a" />
+                    已选中三个文件
+                </div>
+                <div className="checkThree">
+                    <div className="checkDiv">
+                        <input onClick={(e)=>this.hadleCheck(e,pictures[0].id)} className="checkbox" checked= {arr.indexOf(pictures[0].id)!==-1?"checked":""}  type="checkbox" value={pictures[0].id} />
+                        <img src={pictures[0].url}/>
+                    </div>
+                    <div className="checkDiv">
+                        <input onClick={(e)=>this.hadleCheck(e,pictures[1].id)} className="checkbox" checked= {arr.indexOf(pictures[1].id)!==-1?"checked":""} type="checkbox" value={pictures[1].id}/>
+                        <img src={pictures[1].url}/>
+                    </div>
+                    <div className="checkDiv">
+                        <input onClick={(e)=>this.hadleCheck(e,pictures[2].id)} className="checkbox" checked= {arr.indexOf(pictures[2].id)!==-1?"checked":""} type="checkbox" value={pictures[2].id} />
+                        <img src={pictures[2].url}/>
+                    </div>
+                </div>
+             </div>
+        )
+    }
+}
